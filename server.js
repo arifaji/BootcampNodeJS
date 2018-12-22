588835e49bbe1885df17571288a4ad41d35091df
Medasaki <medasakisama@gmail.com>
2018-12-22 13:16:16 +0700

    first

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser');
+    //morgan
+    morgan = require('morgan');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//==tambahan
+var logger = require("./util/logging/winston-logger");
+
+app.use(morgan('combined',{ "stream": logger.stream }));
+logger.debug("Overriding 'Express' logger");
+//end-tambahan
 
 var routesCustomer = require('./router/routes-customer');
 routesCustomer(app);
@@ -14,4 +22,4 @@ var routerTransaction = require('./router/routes-transaction');
 routerTransaction(app);
 
 app.listen(port);
-console.log('Learn Node JS With Kiddy, RESTful API server started on: ' + port);
+logger.debug('Learn Node JS With Kiddy, RESTful API server started on: ' + port);
